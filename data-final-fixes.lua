d3820640ce81de8e863635a769c6589c91d04851
Glendening <nicholasneko@yahoo.com>
2020-10-12 07:47:20 -0500

    Create data-final-fixes.lua

---
+++ data-final-fixes.lua
@@ -0,0 +1,10 @@
+if mods["Krastorio2"] and ["GunEqupment"] then  
+    table.insert(data.raw[personal-turret-equipment].categories, "universal-equipment")
+end
+
+if mods["bobvehicleequipment"] and ["SpidertronEngineer"] then
+	table.insert(data.raw["equipment-grid"]["spidertron-engineer-equipment-grid-2"].equipment_categories, "vehicle-equipment"))
+	table.insert(data.raw["equipment-grid"]["spidertron-engineer-equipment-grid-3"].equipment_categories, "vehicle-equipment"))
+	table.insert(data.raw["equipment-grid"]["spidertron-engineer-equipment-grid-4"].equipment_categories, "vehicle-equipment"))
+	table.insert(data.raw["equipment-grid"]["spidertron-engineer-equipment-grid-5"].equipment_categories, "vehicle-equipment"))
+end
